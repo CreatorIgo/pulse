5fd5696774502161cc51933c6f6a0a8338ef5981
CreatorIgo <posttis@yandex.ru>
2021-11-09 16:17:18 +0500

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
-        .pipe(autoprefixer())
+        .pipe(autoprefixer())                           // берет настройики из package.json                  
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
