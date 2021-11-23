4d8a03ceeaedcf9a03d123bf6a555ca2445d5b04
CreatorIgo <posttis@yandex.ru>
2021-11-23 15:17:56 +0500

    add slider tiny-slider excample

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+ /*    slick - слайдер
 
 // шаблон слайдера скопирован https://kenwheeler.github.io/slick/#go-get-it
 // Initialize your slider in your script file or an inline script tag
@@ -5,28 +6,54 @@
 // документ,  когда струткура документа будет готова
 $(document).ready
     (function(){
-      $('.carousel__inner').slick(
-        {   speed: 700,
-           //dots: true,                 // точки навигации
-            adaptiveHeight: true,         // адаптивная высота под каждый слайд
-            // autoplay: true,             // автопрокрутка
-            // autoSpeed: 300,      
-            fade: true,                   // эффект растворения картинки
-            cssEase: 'linear',             // равномерное растворение
+       
+        $('.carousel__inner').slick(
+          {   speed: 700,
+            //dots: true,                 // точки навигации
+              adaptiveHeight: true,         // адаптивная высота под каждый слайд
+              // autoplay: true,             // автопрокрутка
+              // autoSpeed: 300,      
+              fade: true,                   // эффект растворения картинки
+              cssEase: 'linear',             // равномерное растворение
 
-            // создаем класс html c помощью css
-            prevArrow: '<button type="button" class="slick-prev"> <img src="icons/left.svg"> </button>',
-            nextArrow: '<button type="button" class="slick-next""> <img src="icons/right.svg"> </button>',
-            
-            responsive: [                         // адаптация под различные расширения экрана
-              {
-                  breakpoint: 992,                //  мобильная версия планшета 768px
-                  settings: {
-                      dots: true,                 // точки навигации 
-                      arrows: false               // стрелки навигации 
-                  }
-              }
-            ]
-        }      
-      );
-    });
+              // создаем класс html c помощью css
+              prevArrow: '<button type="button" class="slick-prev"> <img src="icons/left.svg"> </button>',
+              nextArrow: '<button type="button" class="slick-next""> <img src="icons/right.svg"> </button>',
+              
+              responsive: [                         // адаптация под различные расширения экрана
+                {
+                    breakpoint: 992,                //  мобильная версия планшета 768px
+                    settings: {
+                        dots: true,                 // точки навигации 
+                        arrows: false               // стрелки навигации 
+                    }
+                }
+              ]
+          }      
+        );
+
+      }); */
+
+
+      // tiny - slider 
+      const slider = tns({
+        container: '.carousel__inner',     // обращение может быть так же по id "container": "#base",
+        items: 1,                          // слайдов отобр. на экране
+        slideBy: 'page',                   // кол-во перелистываемых слайдов 
+        // nav: false,                     // точки навигации
+        controls: false                    // стандартные стрелки
+        // autoplay: false,
+        // стрелки создаваемые скриптом из документации tiny
+        // controlsText: [
+        //     '<img src="icons/left.svg">',
+        //     '<img src="icons/right.svg">'
+        //   ]
+      });
+
+      // обработчик событий onClick мыши  на кнопки навигации 'следующий', 'предидущий' слайд
+      document.querySelector('.next').addEventListener('click',  function ()  {
+        slider.goTo('next');
+      } );
+      document.querySelector('.prev').addEventListener('click',  function ()  {
+        slider.goTo('prev');
+      });
